414fe6e8ed16c46bb1714b794b3d0c4a639a4f43
Curacerto <curacerto@gmail.com>
2024-10-19 19:08:11 +0100

    Price Table creation

---
+++ dinos/updates.sql
@@ -0,0 +1,23 @@
+UPDATE `dinosaur` SET `name` = 'T_REX' WHERE `id` = 1;
+UPDATE `dinosaur` SET `name` = 'GIGANOTOSAURUS' WHERE `id` = 2;
+UPDATE `dinosaur` SET `name` = 'CARCHARODONTOSAURUS' WHERE `id` = 3;
+UPDATE `dinosaur` SET `name` = 'DEINONYCHUS' WHERE `id` = 4;
+UPDATE `dinosaur` SET `name` = 'MANAGARMR' WHERE `id` = 5;
+UPDATE `dinosaur` SET `name` = 'WYVERN_LIGHTNING' WHERE `id` = 6;
+UPDATE `dinosaur` SET `name` = 'SHADOWMANE' WHERE `id` = 7;
+UPDATE `dinosaur` SET `name` = 'REAPER' WHERE `id` = 8;
+UPDATE `dinosaur` SET `name` = 'MEGATHERIUM' WHERE `id` = 9;
+UPDATE `dinosaur` SET `name` = 'THERIZINOSAURUS' WHERE `id` = 10;
+UPDATE `dinosaur` SET `name` = 'ROCK_DRAKE' WHERE `id` = 11;
+UPDATE `dinosaur` SET `name` = 'DESMODUS' WHERE `id` = 12;
+UPDATE `dinosaur` SET `name` = 'OVIS' WHERE `id` = 13;
+UPDATE `dinosaur` SET `name` = 'YUTYRANNUS' WHERE `id` = 14;
+
+UPDATE `stat_count` SET `name` = '2 STATUS' WHERE `id` = 1;
+UPDATE `stat_count` SET `name` = '4 STATUS' WHERE `id` = 2;
+
+UPDATE `stat_range` SET `name` = 'RANGE 1' WHERE `id` = 1;
+UPDATE `stat_range` SET `name` = 'RANGE 2' WHERE `id` = 2;
+UPDATE `stat_range` SET `name` = 'RANGE 3' WHERE `id` = 3;
+UPDATE `stat_range` SET `name` = 'RANGE 4' WHERE `id` = 4;
+UPDATE `stat_range` SET `name` = 'RANGE 5' WHERE `id` = 5;
