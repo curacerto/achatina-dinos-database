a9199e9c1c4055798f1222f2cc2dde78e5273795
Curacerto <curacerto@gmail.com>
2025-02-05 07:21:42 +0000

    backup

---
+++ backups/20250205/order.sql
@@ -0,0 +1,58 @@
+create table `order`
+(
+    id           int auto_increment
+        primary key,
+    player_id    int                                 not null,
+    resource_id  int                                 null,
+    msg_id       varchar(256)                        null,
+    amount       int                                 not null,
+    price        int                                 not null,
+    total        int                                 not null,
+    map          varchar(100)                        null,
+    status       varchar(100)                        null,
+    created_at   timestamp default CURRENT_TIMESTAMP not null,
+    kit_id       int                                 null,
+    dinosaur_id  int                                 null,
+    saddle_id    int                                 null,
+    is_blueprint tinyint(1)                          null,
+    constraint fk_order_player_id
+        foreign key (player_id) references player (id)
+);
+
+create index fk_order_resource_id
+    on `order` (resource_id);
+
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (1, 1, 40, '1333142644846493741', 1, 3000, 3000, '', 'completed', '2025-01-26 18:32:49', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (2, 1, 23, '1333142872941400115', 1, 3000, 3000, '', 'completed', '2025-01-26 18:33:44', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (3, 1, 21, '1333143048317833408', 1, 2000, 2000, '', 'completed', '2025-01-26 18:34:27', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (4, 1, 4, '1333143363913781388', 1, 1000, 1000, '', 'completed', '2025-01-26 18:35:42', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (5, 1, 25, '1333143935761256528', 1, 3000, 3000, '', 'completed', '2025-01-26 18:37:59', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (6, 1, 14, '1333144210093768757', 1, 2000, 2000, '', 'completed', '2025-01-26 18:39:04', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (7, 1, 41, '1333157502098997310', 1, 1000, 1000, '', 'completed', '2025-01-26 19:31:53', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (8, 1, 19, '1333172992645271574', 1, 2000, 2000, '', 'completed', '2025-01-26 20:33:26', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (9, 1, 25, '1333173217388527628', 1, 3000, 3000, '', 'completed', '2025-01-26 20:34:18', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (10, 1, 13, '1333173509265821816', 1, 2000, 2000, '', 'completed', '2025-01-26 20:35:29', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (11, 1, 56, '1333191757101531208', 1, 6000, 6000, '', 'completed', '2025-01-26 21:48:01', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (12, 18, 0, '1333212461020745851', 1, 5000, 5000, '', 'completed', '2025-01-26 23:10:11', 1, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (13, 18, 0, '1333212684732338207', 1, 5000, 5000, '', 'completed', '2025-01-26 23:11:10', 1, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (14, 18, 0, '1333212951745921109', 1, 5000, 5000, '', 'completed', '2025-01-26 23:12:11', 1, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (15, 25, 0, '1333805706285027402', 1, 5000, 5000, '', 'completed', '2025-01-28 14:27:29', 1, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (16, 25, 0, '1333815475884392578', 1, 5000, 5000, '', 'completed', '2025-01-28 15:06:25', 1, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (17, 1, 24, '1333887771055620106', 1, 3000, 3000, '', 'completed', '2025-01-28 19:53:39', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (18, 1, 23, '1333887911321538570', 1, 3000, 3000, '', 'completed', '2025-01-28 19:54:16', 0, null, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (19, 1, 0, '1335021042090901637', 1, 3000, 3000, '', 'canceled', '2025-01-31 22:56:51', 0, 49, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (20, 1, 0, '1335021212715454516', 1, 3000, 3000, '', 'completed', '2025-01-31 22:57:35', 0, 49, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (21, 1, 0, '1335564970145878098', 1, 5000, 5000, '', 'completed', '2025-02-02 10:58:14', 0, 66, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (22, 3, 28, '1335689027076231179', 1, 3000, 3000, '', 'completed', '2025-02-02 19:11:04', 0, 0, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (23, 27, 0, '1335823483116064849', 1, 5000, 5000, '', 'map_pending', '2025-02-03 04:05:37', 1, 0, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (24, 20, 0, '1335874228767035406', 1, 5000, 5000, '', 'completed', '2025-02-03 07:27:08', 0, 61, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (25, 20, 0, '1335875092407779378', 1, 2000, 2000, '', 'completed', '2025-02-03 07:30:37', 0, 20, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (26, 20, 0, '1335875345475309608', 1, 2000, 2000, '', 'completed', '2025-02-03 07:31:38', 0, 20, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (27, 20, 0, '1335888951075934238', 1, 5000, 5000, '', 'completed', '2025-02-03 08:25:39', 0, 64, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (28, 20, 0, '1335890550641004556', 1, 5000, 5000, '', 'completed', '2025-02-03 08:32:03', 0, 64, null, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (29, 1, 0, '1336422342884331633', 1, 0, 0, '', 'completed', '2025-02-04 19:45:09', 0, 0, 1, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (30, 1, 0, '1336444109140787230', 1, 0, 0, '', 'completed', '2025-02-04 21:11:36', 0, 0, 1, null);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (31, 23, 0, '1336477372668706877', 1, 5000, 5000, '', 'completed', '2025-02-04 23:23:51', 0, 36, 0, 0);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (32, 23, 0, '1336493698984443956', 1, 5000, 5000, '', 'completed', '2025-02-05 00:28:43', 0, 69, 0, 0);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (33, 23, 0, '1336494147762258042', 1, 5000, 5000, '', 'completed', '2025-02-05 00:30:30', 0, 69, 0, 0);
+INSERT INTO dinos.`order` (id, player_id, resource_id, msg_id, amount, price, total, map, status, created_at, kit_id, dinosaur_id, saddle_id, is_blueprint) VALUES (34, 23, 0, '1336494648784453702', 1, 5000, 5000, '', 'completed', '2025-02-05 00:32:28', 0, 69, 0, 0);
---
+++ backups/20250205/transfer.sql
@@ -0,0 +1,76 @@
+create table transfer
+(
+    id            int auto_increment
+        primary key,
+    source_id     int                                 not null,
+    target_id     int                                 not null,
+    amount        int                                 not null,
+    created_at    timestamp default CURRENT_TIMESTAMP not null,
+    transfer_type varchar(100)                        null,
+    constraint fk_transfer_source_id
+        foreign key (source_id) references player (id),
+    constraint fk_transfer_target_id
+        foreign key (target_id) references player (id)
+);
+
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (1, 1, 16, 85000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (2, 16, 1, 60000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (3, 1, 6, 60000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (4, 1, 6, 10000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (5, 2, 1, 10000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (6, 3, 1, 10000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (7, 2, 3, 90000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (8, 1, 3, 167000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (9, 1, 18, 40000, '2025-01-15 14:28:01', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (10, 18, 1, 40000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (11, 8, 1, 3000, '2025-01-15 14:28:01', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (12, 20, 1, 200000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (13, 3, 4, 90000, '2025-01-15 14:28:00', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (14, 20, 2, 100000, '2025-01-15 18:47:19', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (15, 1, 20, 125000, '2025-01-17 21:03:43', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (16, 20, 1, 10000, '2025-01-17 22:25:32', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (17, 1, 21, 30000, '2025-01-18 01:16:15', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (18, 21, 1, 5000, '2025-01-18 22:16:45', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (19, 20, 1, 60000, '2025-01-19 12:47:48', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (20, 1, 23, 10000, '2025-01-20 18:45:04', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (21, 6, 1, 25000, '2025-01-20 22:16:50', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (22, 21, 20, 5000, '2025-01-20 22:22:49', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (23, 20, 2, 60000, '2025-01-22 21:19:16', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (24, 1, 20, 300000, '2025-01-25 23:32:19', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (25, 1, 22, 25000, '2025-01-25 23:51:34', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (26, 22, 1, 25000, '2025-01-25 23:53:24', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (27, 20, 2, 260000, '2025-01-26 10:29:16', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (28, 1, 18, 5000, '2025-01-26 23:09:27', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (29, 18, 1, 5000, '2025-01-26 23:10:28', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (30, 1, 18, 5000, '2025-01-26 23:10:53', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (31, 18, 1, 5000, '2025-01-26 23:11:19', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (32, 1, 18, 5000, '2025-01-26 23:12:07', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (33, 18, 1, 5000, '2025-01-26 23:12:22', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (34, 1, 24, 100000, '2025-01-26 23:52:48', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (35, 1, 25, 35000, '2025-01-28 14:22:26', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (36, 25, 1, 5000, '2025-01-28 14:27:54', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (37, 25, 1, 25, '2025-01-28 15:02:57', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (38, 25, 1, 24975, '2025-01-28 15:05:02', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (39, 25, 1, 5000, '2025-01-28 15:06:35', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (40, 1, 2, 10000, '2025-01-28 19:52:13', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (41, 2, 1, 10000, '2025-01-28 19:52:59', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (42, 20, 2, 25000, '2025-01-28 20:24:27', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (43, 1, 14, 5000, '2025-01-28 23:39:41', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (44, 24, 1, 36000, '2025-01-28 23:47:12', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (45, 24, 1, 10000, '2025-01-29 00:05:16', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (46, 1, 26, 50000, '2025-02-01 11:00:15', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (47, 1, 27, 10000, '2025-02-01 11:04:26', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (48, 3, 1, 3000, '2025-02-02 19:11:35', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (49, 1, 22, 40000, '2025-02-03 00:52:46', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (50, 20, 1, 0, '2025-02-03 07:27:41', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (51, 20, 1, 0, '2025-02-03 07:30:45', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (52, 20, 1, 0, '2025-02-03 07:31:45', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (53, 20, 1, 0, '2025-02-03 08:25:49', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (54, 20, 1, 0, '2025-02-03 08:32:10', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (55, 1, 25, 45000, '2025-02-03 22:00:28', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (56, 25, 1, 45, '2025-02-03 22:43:47', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (57, 25, 1, 44955, '2025-02-03 22:44:39', 'transfer');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (58, 23, 1, 0, '2025-02-04 23:24:02', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (59, 23, 1, 0, '2025-02-05 00:28:52', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (60, 23, 1, 0, '2025-02-05 00:30:39', 'order');
+INSERT INTO dinos.transfer (id, source_id, target_id, amount, created_at, transfer_type) VALUES (61, 23, 1, 0, '2025-02-05 00:32:38', 'order');
