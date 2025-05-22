33bd42209a305bceda8a95fee2fe8ad1324c60fe
Curacerto <curacerto@gmail.com>
2025-05-22 10:33:39 +0100

    player tick and income

--- dinos/player_income.sql
+++ dinos/player_income.sql
@@ -1,10 +1,12 @@
-create table player_tick
+create table player_income
 (
     id             int auto_increment
         primary key,
     player_id      int not null,
     amount         int null,
+    balance_before int null,
+    balance_after  int null,
     created_at     timestamp,
-    constraint fk_player_tick_player_id
+    constraint fk_player_income_player_id
         foreign key (player_id) references player (id)
 );
